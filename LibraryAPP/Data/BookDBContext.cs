f67b8f0d6a5a3a860329b5f841aacf3e1c8ef279
manju <manjunathdesai861@gmail.com>
2025-03-06 20:34:57 +0530

    add

---
+++ LibraryAPP/Data/BookDBContext.cs
@@ -0,0 +1,14 @@
+﻿using LibraryAPP.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryAPP.Data
+{
+    public class BookDBContext:DbContext
+    {
+        public BookDBContext(DbContextOptions<BookDBContext> options):base(options) 
+        {
+
+        }
+        public DbSet<BookModel> AllBooks {  get; set; }
+    }
+}
