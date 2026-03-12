fb46e795f18fe4000a558f6abf5a0820e1a7772e
manju <manjunathdesai861@gmail.com>
2026-03-12 21:57:22 +0530

    Added all fiels

--- LibraryAPP/obj/Debug/net8.0/LibraryAPP.AssemblyInfo.cs
+++ LibraryAPP/obj/Debug/net8.0/LibraryAPP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryAPP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a715a7a6f3a53685c861abb87230502f5cad89bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d78087fca9c4ab47ad365a04a2d6f4f43aabec0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryAPP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryAPP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
